54164e78fd923ddbc22c80a2529558d60b769664
lostmap <lostmap4@gmail.com>
2020-05-21 02:56:26 +0300

    exceptions handling

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
 import boto3
-
+import botocore
 # Retrieve the list of existing buckets
 s3 = boto3.client('s3', region_name="RegionOne", verify=False,
                         endpoint_url="https://localhost",
                         aws_access_key_id="2dff69e63984445c819a1a3c3328bf8b",
                         aws_secret_access_key="9bbe336440c94b079a19bb7b89aec749")
-response = s3.list_buckets()
-print response
+try:
+    response = s3.list_buckets()
+    print response
+except botocore.exceptions.ConnectionError as error:
+    print "ERROR what"
+    raise error
